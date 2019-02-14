7e95e62654dc95d981cc3986a3a8f238a06fcc3d
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2019-02-14 21:12:44 +0700

    No clang, no msvc

--- project/configure.sh
+++ project/configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Warning: Please check that you have already installed C/C++ and Fortran compiler"
-echo "To install C/C++ compiler: Install gcc, g++ or clang"
+echo "To install C/C++ compiler: Install gcc and g++"
 echo "To install Fortran compiler: Install gfortran"
 read -r -p "Continue? [y/N] " response;
 if [ -z "$(echo $response | grep -E "^([yY][eE][sS]|y)$")" ]; then
