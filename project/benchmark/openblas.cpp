7e69b2cbb5b5e9633fa065810a557c8a09394c9f
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2019-02-19 17:19:06 +0700

    Add OpenBLAS (failed)

---
+++ project/benchmark/openblas.cpp
@@ -0,0 +1,29 @@
+#include <memory>
+
+#include <benchmark/benchmark.h>
+//#include <cblas.h>
+
+static void BM_OpenBLAS_Matmul(benchmark::State &state) {
+  /*
+  std::srand(std::time(0));
+  int M = state.range(0);
+  int K = state.range(1);
+  int N = state.range(2);
+  std::unique_ptr<double[]> A(new double[M * K]);
+  std::unique_ptr<double[]> B(new double[K * N]);
+  std::unique_ptr<double[]> C(new double[M * N]);
+  for (auto _ : state) {
+    state.PauseTiming();
+    for (int i = 0; i < M * K; i++)
+      A[i] = 1.0 / std::rand();
+    for (int i = 0; i < K * N; i++)
+      B[i] = 1.0 / std::rand();
+    state.ResumeTiming();
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, M, N, K, 1.0,
+                A.get(), M, B.get(), K, 0.0, C.get(), M);
+  }
+  */
+}
+BENCHMARK(BM_OpenBLAS_Matmul)
+    ->Args({100, 150, 200})
+    ->Args({1000, 1500, 2000});
