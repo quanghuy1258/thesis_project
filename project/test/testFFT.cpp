084952686b78ffd09ab32d490f009c5667943f13
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2019-03-22 17:06:22 +0700

    Fix test FFT in T_N[X]

--- project/test/testFFT.cpp
+++ project/test/testFFT.cpp
@@ -7,19 +7,26 @@
 TEST(Thesis, FFT) {
   std::srand(std::time(nullptr));
   thesis::FFT fftObj;
-  ASSERT_TRUE(fftObj.set_N(8));
-  std::vector<thesis::Integer> polyA(8);
-  std::vector<thesis::Torus> polyB(8), polyResult(8);
-  for (int i = 0; i < 8; i++) {
+  int deg = 8;
+  ASSERT_TRUE(fftObj.set_N(deg));
+  std::vector<thesis::Integer> polyA(deg);
+  std::vector<thesis::Torus> polyB(deg), polyResult(deg), polyTest(deg);
+  for (int i = 0; i < deg; i++) {
     polyA[i] = std::rand();
     polyB[i] = std::rand();
   }
-  ASSERT_TRUE(fftObj.torusPolynomialMultiplication(polyResult, polyA, polyB));
-  for (int i = 0; i < 8; i++) {
-    thesis::Torus temp = 0;
-    for (int j = 0; j <= i; j++) {
-      temp += polyA[i - j] * polyB[j];
+  std::fill(polyTest.begin(), polyTest.end(), 0);
+  for (int i = 0; i < deg; i++) {
+    for (int j = 0; j < deg; j++) {
+      if (i + j < deg) {
+        polyTest[i + j] += polyA[i] * polyB[j];
+      } else {
+        polyTest[i + j - deg] -= polyA[i] * polyB[j];
+      }
     }
-    ASSERT_TRUE(polyResult[i] == temp);
+  }
+  ASSERT_TRUE(fftObj.torusPolynomialMultiplication(polyResult, polyA, polyB));
+  for (int i = 0; i < deg; i++) {
+    ASSERT_TRUE(polyResult[i] == polyTest[i]);
   }
 }
